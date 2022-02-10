568439f57fcf1e35377f8c3b715e0933c919d2f7
dylanbentonembrey <embreydb@g.cofc.edu>
2022-02-09 23:55:11 -0500

    Completed lab4.py

---
+++ labs/lab4/lab4.py
@@ -0,0 +1,18 @@
+"""
+Dylan Benton Embrey
+2/9/2022
+Computer Programming Labs 4: Greeting Card
+I certify that this is my work.
+"""
+from graphics import Circle, Point, GraphWin, Text, Image
+
+
+def greeting_card():
+    win = GraphWin('Greeting Card!', 700, 700)
+    win.setCoords(10, 10, 100, 100)
+    greeting_card_text = Text(Point(50, 50), "Happy Valentine's Day!!!")
+    greeting_card_text.draw(win)
+    win.getMouse()
+
+
+greeting_card()
